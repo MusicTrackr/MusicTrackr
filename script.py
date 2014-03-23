e95a5cdda0e09c40654eb350856f60e944e52183
Kai Kuehner <kvk@pobox.com>
2014-03-22 21:27:43 -0400

    get albums

--- script.py
+++ script.py
@@ -10,17 +10,27 @@ def init():
 	#print(artists)
 	main()
 
+def get_json(url):
+	page = requests.get(url)
+	txt = page.text
+	dat = json.loads(txt)
+	return dat
+
 def get_artist_id(name):
 	#from input get artist id
 	name = name.replace(" ", "+")
-	page = requests.get("https://itunes.apple.com/search?term=" + name)
-	txt = page.text
-	print(txt)
-	dat = json.loads(txt)
+	dat = get_json("https://itunes.apple.com/search?term=" + name)
 	return dat["results"][0]["artistId"]
 
 #from artist id get albums
-
+def get_albums(id):
+	#id is a string
+	albums = []
+	dat = get_json("https://itunes.apple.com/lookup?id=" + id + "&entity=album")
+	for result in dat["results"]:
+		if result["wrapperType"] == "collection":
+			albums.append(result)
+	return albums
 
 def main():
 	'''while True:
