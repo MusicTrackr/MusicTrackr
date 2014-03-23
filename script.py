d0bc68c9f4060706aff1884ce9132ab6c2c67944
Kai Kuehner <kvk@pobox.com>
2014-03-22 20:28:39 -0400

    fill in sendgrid method

--- script.py
+++ script.py
@@ -1,17 +1,31 @@
 import sendgrid
 import json
+
 artists = {}
+
 def init():
 	artistf = open('artists.txt','r+')
 	artists = artistf.read().strip()
 	#print(artists)
 	main()
+
 def main():
 	'''while True:
 		for artist in artists:
 			for subscriber in artist['subscribers']:
 				if artist['newalbum'] is not False:
 					mailuser(subscriber['email'],artist,newalbum)'''
-#def mailuser(address,artist,newalbum):
-	#TODO
+
+def mailuser(address, artist, newalbum):
+	#address: email address (string)
+	#artist: artist's name (string)
+	#newalbum: link to new album (string)
+	s = sendgrid.SendGridClient('parasm', 'bcabooks')
+	msg = sendgrid.Mail()
+	msg.add_to(address)
+	msg.set_subject("MusicTrackr: New Album Release")
+	msg.set_html("A new album has been released by " + artist + "! Find it on <a href=\"" + newalbum + "\">iTunes</a>.")
+	msg.set_from("notif@musictrackr.com")
+	status, msg = s.send(message)
+
 init()
