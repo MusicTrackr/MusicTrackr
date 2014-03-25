2d97a8c3f4c22c1e3e7854602df4af17afcc7334
Ezra Brooks <ezra.m.brooks@gmail.com>
2014-03-25 09:48:25 -0400

    Hungarian notation, d00d

--- script.py
+++ script.py
@@ -12,10 +12,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 def init():
-	global githuburl
+	global githubUrl
 	global artistf
 	global artists
-	githuburl = 'https://www.github.com/kaikue/MusicAlert'
+	githubUrl = 'https://www.github.com/kaikue/MusicAlert'
 	try:
 		artistf = open('artists.txt','r+')
 		artists = literal_eval(artistf.read().strip())
@@ -33,14 +33,14 @@ def init():
 	t.start()
 	app.run(debug=True)
 
-def subscribe(artist_name, email):
-	id = get_artist_id(artist_name)
+def subscribe(artistName, email):
+	id = getArtistId(artistName)
 	if id not in artists.keys():
-		artists[id] = {"subscribers":[], "albums":get_albums(id)}
+		artists[id] = {"subscribers":[], "albums":getAlbums(id)}
 	if email not in artists[id]['subscribers']:
 		artists[id]['subscribers'].append(email)
 	if 'testing' in globals() and testing is True:
-		artists[get_artist_id(artist_name)]["albums"].pop(0)
+		artists[getArtistId(artistName)]["albums"].pop(0)
 		update()
 	else:
 		artistf.truncate()
@@ -49,41 +49,41 @@ def subscribe(artist_name, email):
 def update():
 	#run this every morn
 	for artist in artists:
-		old_albums = artists[artist]['albums']
-		new_albums = get_albums(artist)
-		for new_album in new_albums:
-			if new_album not in old_albums:
+		oldAlbums = artists[artist]['albums']
+		newAlbums = getAlbums(artist)
+		for newAlbum in newAlbums:
+			if newAlbum not in oldAlbums:
 				for subscriber in artists[artist]['subscribers']:
-					mailuser(subscriber, new_album['artistName'], new_album['collectionName'], new_album['collectionViewUrl'])
-		artists[artist]['albums'] = new_albums
+					mailUser(subscriber, newAlbum['artistName'], newAlbum['collectionName'], newAlbum['collectionViewUrl'])
+		artists[artist]['albums'] = newAlbums
 	artistf.truncate()
 	artistf.write(str(artists))
 
-def get_json(url):
+def getJson(url):
 	page = urllib.request.urlopen(url)
 	return json.loads(page.read().decode(page.headers.get_content_charset()))
 
-def get_artist_id(name):
+def getArtistId(name):
 	#from input get artist id
-	plus_name = name.replace(" ", "+")
-	dat = get_json("https://itunes.apple.com/search?term=" + plus_name + '&entity=musicArtist')
+	plusName = name.replace(" ", "+")
+	dat = getJson("https://itunes.apple.com/search?term=" + plusName + '&entity=musicArtist')
 	for i in range(0,len(dat['results'])-1):
 		if dat["results"][i]["artistName"].lower() == name.lower():
 			artistId = dat['results'][i]['artistId']
 			return artistId
 
 #from artist id get albums
-def get_albums(id):
+def getAlbums(id):
 	#id is NOT a string
 	albums = []
-	dat = get_json("https://itunes.apple.com/lookup?id=" + str(id) + "&entity=album")
+	dat = getJson("https://itunes.apple.com/lookup?id=" + str(id) + "&entity=album")
 	for result in dat["results"]:
 		if result["wrapperType"] == "collection":
 			del result['copyright']
 			albums.append(result)
 	return albums
 
-def mailuser(address, artist, album, url):
+def mailUser(address, artist, album, url):
 	#address: email address (string)
 	#artist: artist's name (string)
 	#album: name of album (string)
@@ -137,6 +137,11 @@ def artiststxt():
 	artistf.flush()
 	afcontents = artistf.read()
 	return afcontents'''
+
+@app.route('/artist')
+def artistPage():
+	artistName = request.args['artist']
+	print(artistName)
 
 if __name__ == '__main__':
 	init()
